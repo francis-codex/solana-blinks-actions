28755ffd060b019c1fc2aae1f230cc75b39334bd
francis-codex <francislogo2219@gmail.com>
2024-08-23 23:00:04 +0100

    feat: Implemented the first response

--- web/app/api/hello/route.ts
+++ web/app/api/hello/route.ts
@@ -1,3 +1,14 @@
-export async function GET(request: Request) {
-  return new Response('Hello, from API!');
+import { ActionGetRequest} from "@solana/actions"
+
+export async function GET(request: Request) {    
+  const response : ActionGetRequest = {
+    icons: "/sprintiq.jpg",
+    description: " This is a demo blink",
+    title: " Do Blink",
+    lable: " Click me ",
+    error: {
+      message: "Ivalid blinks",
+    },
+  }  
+  return Response.json({response})
 }

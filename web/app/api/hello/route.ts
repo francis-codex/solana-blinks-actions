168ff5f589a657c6959e73f3ae64459ae41a5a48
francis-codex <francislogo2219@gmail.com>
2024-08-23 23:28:07 +0100

    feat: added POST request

--- web/app/api/hello/route.ts
+++ web/app/api/hello/route.ts
@@ -1,4 +1,4 @@
-import { ActionGetRequest} from "@solana/actions"
+import { ActionGetRequest, ActionPostRequest, ActionPostResponse} from "@solana/actions"
 
 export async function GET(request: Request) {    
   const response : ActionGetRequest = {
@@ -9,6 +9,22 @@ export async function GET(request: Request) {
     error: {
       message: "Ivalid blinks",
     },
+  }  
+  return Response.json({response})
+}
+
+
+export async function POST(request: Request) {   
+  
+  const postRequest: ActionPostRequest  = await request.json();
+  const userPubkey = postRequest.account;
+  console.log(userPubkey);
+
+  const response : ActionPostResponse = {
+
+    transaction: "",
+    message: "This is "+userPubkey,
+   
   }  
   return Response.json({response})
 }
